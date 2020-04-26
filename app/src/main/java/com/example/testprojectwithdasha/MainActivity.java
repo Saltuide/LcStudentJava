767c1c9335a10cfc455f33089211231cd8b9afe3
Saltuide <goonerus38@gmail.com>
2020-04-26 21:49:43 +0800

    ну вход типа есть

--- app/src/main/java/com/example/testprojectwithdasha/MainActivity.java
+++ app/src/main/java/com/example/testprojectwithdasha/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testprojectwithdasha;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -15,10 +16,14 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
     private static final int CONNECTION_TIMEOUT = 5000;
     Button registration;
     TextInputEditText password, login;
-
-
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public static String postRequest(String login, String password) throws Exception{
@@ -47,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         String urlParameters = body.toString();
 
         // Send post request
-        con.setDoOutput(true);
         try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
             wr.writeBytes(urlParameters);
             wr.flush();
@@ -62,19 +64,15 @@ public class MainActivity extends AppCompatActivity {
             while ((line = in.readLine()) != null) {
                 response.append(line);
             }
-
             return response.toString();
-
         }
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (android.os.Build.VERSION.SDK_INT > 9)
-        {
+        if (android.os.Build.VERSION.SDK_INT > 9){
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
@@ -82,18 +80,36 @@ public class MainActivity extends AppCompatActivity {
         password  = (TextInputEditText) findViewById(R.id.password);
         login = (TextInputEditText) findViewById(R.id.login);
 
-
-
         View.OnClickListener oclBtn = new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
+                String answer = new String();
                 try {
-                    String s = postRequest(login.getText().toString(), password.getText().toString());
-                    System.out.println(s);
+                    answer = postRequest(login.getText().toString(), password.getText().toString());
+                    answer = answer.replace("\"", "");
+                    System.out.println(answer);
+                    //{"status": true, "is_authorized": false, "id": "dasha@ya.ru"}
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                Properties props = new Properties();
+                try {
+                    props.load(new StringReader(answer.substring(1, answer.length() - 1).replace(", ", "\n")));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Map<String, String> parsedJSON = new HashMap<String, String>();
+                for (Map.Entry<Object, Object> e : props.entrySet()) {
+                    parsedJSON.put((String)e.getKey(), (String)e.getValue());
+                }
+                if(parsedJSON.get("status").equals("true")){
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                }else{
+                    System.out.println("YOU ARE NOT ALLOWED TO CUM");
+                }
+
             }
         };
         registration.setOnClickListener(oclBtn);
